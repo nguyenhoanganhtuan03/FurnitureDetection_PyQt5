4d0ee755fd3e9318f23f01a2cbcc53a86460b6b1
Nguyễn Hoàng Anh Tuấn <127226234+nguyenhoanganhtuan03@users.noreply.github.com>
2024-04-28 11:01:26 +0700

    Commit

---
+++ Model_Yolo/src_code/check_lib_ver.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import torch
+import PyQt5.QtCore
+import nltk
+import keras
+import tensorflow as tf
+import cv2
+import mlxtend
+
+print("numpy version:", np.__version__)
+print("pandas version:", pd.__version__)
+print("torch version:", torch.__version__)
+print("PyQt5 version:", PyQt5.QtCore.QT_VERSION_STR)
+print("nltk version:", nltk.__version__)
+print("keras version:", keras.__version__)
+print("tensorflow version:", tf.__version__)
+print("opencv-python version:", cv2.__version__)
+print("mlxtend version:", mlxtend.__version__)
